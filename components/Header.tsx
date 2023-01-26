ec6330bf7cbdcfaa8bc5e3898ed58e337dd59f27
Koray Guner <okorayguner@gmail.com>
2023-01-26 23:52:51 +0300

    feat: Header component added

---
+++ components/Header.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Logo from '@/assets/Logo'
+import { HiSearch } from "react-icons/hi"
+import { HiBell } from "react-icons/hi"
+import Link from 'next/link'
+
+function Header() {
+    const [ isScrolled, setIsScrolled ] = useState(false)
+
+    useEffect(()=> {
+        const handleScroll = () => {
+            if ( window.scrollY > 0 ) {
+                setIsScrolled(true)
+            }else {
+                setIsScrolled(false)
+            }
+        }
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    },[])
+    
+
+    return (
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
+            {/* Left Section */}
+            <div className='flex items-center space-x-2 md:space-x-10'>
+                {/* Logo svg */}
+                <Logo />
+
+                <ul className='hidden space-x-4 md:flex'>
+                    <li className='headerLink'>Home</li>
+                    <li className='headerLink'>TV Shows</li>
+                    <li className='headerLink'>Movies</li>
+                    <li className='headerLink'>New & Popular</li>
+                    <li className='headerLink'>My List</li>
+                </ul>
+            </div>
+
+            {/* Right Section */}
+            <div className='flex items-center space-x-4 text-sm font-light'>
+                <HiSearch className='hidden sm:inline h-6 w-6'/>
+                <p className='hidden lg:inline'>Kids</p>
+                <HiBell className='h-6 w-6' />
+                <Link href="/account">
+                    <img src="./defaultAvatar.png" alt=""/>
+                </Link>
+            </div>
+
+
+        </header>
+    )
+}
+
+export default Header
