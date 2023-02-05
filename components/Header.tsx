f7de4786c0f217372912bf4690745ff495b3cf39
Koray Guner <okorayguner@gmail.com>
2023-02-05 20:14:14 +0300

    feat: Dropdown component added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,58 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import Logo from '@/assets/Logo'
-import { HiSearch } from "react-icons/hi"
-import { HiBell } from "react-icons/hi"
-import Link from 'next/link'
-import useAuth from '@/hooks/useAuth'
+import React, { useEffect, useState, useRef } from 'react';
+import Logo from '@/assets/Logo';
+import { HiSearch } from 'react-icons/hi';
+import { HiBell } from 'react-icons/hi';
+import Link from 'next/link';
+import useAuth from '@/hooks/useAuth';
+import DropDown from './DropDown';
+import { MdOutlineHelpOutline, MdOutlinePersonOutline } from 'react-icons/md';
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
 
 function Header() {
-    const [ isScrolled, setIsScrolled ] = useState(false)
-    const { logout } = useAuth()
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [dropDownIsOpen, setDropDownIsOpen] = useState(false);
+  const { logout } = useAuth();
+  const menuList = [
+    {
+      name: 'Manage Profiles',
+      icon: <HiOutlinePencilSquare />,
+    },
+    {
+      name: 'Account',
+      icon: <MdOutlinePersonOutline />,
+    },
+    {
+      name: 'Help Center',
+      icon: <MdOutlineHelpOutline />,
+    },
+    {
+      name: 'Sign out of Netflix',
+      onClick: logout,
+    },
+  ];
 
+  const menuRef = useRef();
 
-    useEffect(()=> {
-        const handleScroll = () => {
-            if ( window.scrollY > 0 ) {
-                setIsScrolled(true)
-            }else {
-                setIsScrolled(false)
-            }
-        }
-        window.addEventListener("scroll", handleScroll)
-        return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    },[])
-    
-    return (
-        <header className={`${isScrolled && "bg-[#141414]"} transition duration-200 z-30`}>
-            {/* Left Section */}
-            <div className='flex items-center space-x-2 md:space-x-10'>
-                <Logo />
-                <ul className='hidden space-x-4 md:flex'>
-                    <li className='headerLink'>Home</li>
-                    <li className='headerLink'>TV Shows</li>
-                    <li className='headerLink'>Movies</li>
-                    <li className='headerLink'>New & Popular</li>
-                    <li className='headerLink'>My List</li>
-                </ul>
-            </div>
-            {/* Right Section */}
-            <div className='flex items-center space-x-4 text-sm font-light'>
-                <HiSearch className='hidden sm:inline h-6 w-6'/>
-                <p className='hidden lg:inline'>Kids</p>
-                <HiBell className='h-6 w-6' />
-                {/* <Link href="/account"> */}
-                    <img 
-                        className='cursor-pointer rounded'
-                        src="./defaultAvatar.png" 
-                        alt=""
-                        onClick={logout}
-                    />
-                {/* </Link> */}
-            </div>
-        </header>
-    )
+  window.addEventListener('click', (e) => {
+    if (e.target !== menuRef.current) {
+      setDropDownIsOpen(false);
+    }
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`fixed top-0 z-30 w-full flex justify-between items-center p-4 lg:px-10 lg:py-6 transition-all duration-200  ${
+        isScrolled && 'bg-[#141414]'
+      } `}
+    >
+      {/* Left Section */}
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      {/* Right Section */}
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <HiSearch className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <HiBell className="h-6 w-6" />
+        <div className="relative">
+          <img
+            ref={menuRef}
+            className="cursor-pointer rounded"
+            src="./defaultAvatar.png"
+            alt=""
+            onClick={() => setDropDownIsOpen(!dropDownIsOpen)}
+          />
+          {dropDownIsOpen && <DropDown menuList={menuList} />}
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
